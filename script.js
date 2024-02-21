e97948eda9cb28da2da3e6508000db93075be926
hafizullah1991 <Ahmadshakhi2020@gmail.com>
2024-02-20 21:34:40 -0800

    done

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Hello World!\n==========\n");
+console.log(
+  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+);
+
+// Exercise 1
+const firstName="Hafiz";;
+let lastName ;
+var age; 
+// Exercise 2
+
+lastName = "Hamdard"
+age = "22"
+
+
+// Exercise 3
+let language = "JavaScript";
+let createYear = "1995";
+let isCaseSensetive = "True";
+ console.log(language, createYear, isCaseSensetive)
+// Exercise 4
+let price = "19.99"
+let isOnSale = "false"
+
+let salePercentage = "15"
+let stock = "0"; 
+let inStock = "false"
+let selectedSize = "M"
+console.log(salePercentage, stock, selectedSize);
+
+// Exercise 5
+let title = "Name of the Wind"
+let pageCount = "722"
+let bookMark = "456"
+let author = "Patrick Rothfuss"
+let hasRead = "true"
+console.log(`Title: ${title}`);
+console.log(`The title of my book is: ${title}, and it has ${pageCount} pages, do you read it ${hasRead}`)
